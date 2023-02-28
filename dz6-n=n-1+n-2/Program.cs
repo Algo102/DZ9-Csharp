01d3d7cccbc69d23f72922db4b176d8c9289af2d
Algo <algor102@gmail.comm>
2023-03-01 02:03:05 +0500

    Сумма двух предидущих до N

---
+++ dz6-n=n-1+n-2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
+
+int SummDvuh(int n)
+{
+  if (n == 1 || n == 2) return 1;
+  else return SummDvuh(n - 1) + SummDvuh(n - 2);
+}
+
+int IntNum(string message)
+{
+  Console.WriteLine(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int start = IntNum("Введите начало отсчета ");
+int length = IntNum("Введите окончанее ряда ");
+
+for (int i = start; i < length + 1; i++)
+{
+  Console.WriteLine(i + " = " + SummDvuh(i));
+}
+
+
