f91d7ccf8fc4a0bfe56c44108f047ef44e5ab9e7
Algo <algor102@gmail.comm>
2023-02-28 21:42:57 +0500

    Сумма цифр числа

---
+++ dz3-summChisla/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму цифр числа
+int SumNumbers(int number)
+{
+  if (number < 1) return 0;
+  return SumNumbers(number / 10) + number % 10;
+}
+
+int IntNum(string message)
+{
+  Console.WriteLine(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int number = IntNum("Введите число ");
+Console.Write(SumNumbers(number));
