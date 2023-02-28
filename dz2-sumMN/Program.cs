631c7e795a730a0a446e328139cb2d27a516f2be
Algo <algor102@gmail.comm>
2023-02-28 21:36:08 +0500

    Сумма цифр от M до N

---
+++ dz2-sumMN/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+int SumNumber(int M, int N)
+{
+
+  if (N < M)
+    return 0;
+  else
+  {
+    return N + SumNumber(M, N - 1);
+  }
+
+}
+
+int IntNum(string message)
+{
+  Console.WriteLine(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int M = IntNum("Введите M ");
+int N = IntNum("Введите N ");
+Console.WriteLine("Сумма элементов " + SumNumber(M, N));
+
