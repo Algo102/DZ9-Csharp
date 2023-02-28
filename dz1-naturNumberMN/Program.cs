d4f0f7a7f0d3de11a0b64a88e77178e552bd374b
Algo <algor102@gmail.comm>
2023-02-28 20:40:01 +0500

    Натуральные числа от M до N

---
+++ dz1-naturNumberMN/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+void NaturNumb(int m, int n)
+{
+  if (m >= n)
+  {
+    Console.Write(m + " ");
+    NaturNumb(m - 1, n);
+  }
+}
+
+int IntNum(string message)
+{
+  Console.WriteLine(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int m = IntNum("Введите M ");
+int n = IntNum("Введите N ");
+NaturNumb(m, n);
